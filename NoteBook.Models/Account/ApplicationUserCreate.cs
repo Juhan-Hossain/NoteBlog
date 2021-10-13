54a51e8e911a0731ae7b81bf5d44cca08845b5f5
Juhan Hossain <Juhanhossain@gmail.com>
2021-10-13 20:24:01 +0600

    Added Account model

---
+++ NoteBook.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NoteBook.Models.Account
+{
+    public class ApplicationUserCreate
+    {
+        [MaxLength(30, ErrorMessage = "Fullname should between 5-20 chars")]
+        [MinLength(10, ErrorMessage = "Fullname should between 5-20 chars")]
+        public string Fullname { get; set; }
+        [Required(ErrorMessage ="Please provide Email address")]
+        [MaxLength(30, ErrorMessage = "Email should between 5-20 chars")]
+        [EmailAddress(ErrorMessage = "Email format invalid")]
+        public string Email { get; set; }
+    }
+}
---
+++ NoteBook.Models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NoteBook.Models.Account
+{
+    public class ApplicationUserLogin
+    {
+        [Required(ErrorMessage = "Username is required!!!")]
+        [MaxLength(20,ErrorMessage ="length should between 5-20 chars")]
+        [MinLength(5, ErrorMessage = "length should between 5-20 chars")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required!!!")]
+        [MaxLength(50, ErrorMessage = "length should between 8-50 chars")]
+        [MinLength(8, ErrorMessage = "length should between 8-50 chars")]
+        public string Password { get; set; }
+    }
+}
