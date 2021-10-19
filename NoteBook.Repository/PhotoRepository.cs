aa2b8c858d6c81a3c3a5ce9e033bb0b0ca61eab2
Juhan Hossain <Juhanhossain@gmail.com>
2021-10-19 15:10:13 +0600

    photo repository final created

--- NoteBook.Repository/PhotoRepository.cs
+++ NoteBook.Repository/PhotoRepository.cs
@@ -25,7 +25,7 @@ namespace NoteBook.Repository
             using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
             {
                 await connection.OpenAsync();
-                affectedRows = await connection.ExecuteScalarAsync<int>(
+                affectedRows = await connection.ExecuteAsync(
                     "Photo_Delete",new { PhotoId = photoId },commandType:CommandType.StoredProcedure);
             }
             return affectedRows;
@@ -33,17 +33,55 @@ namespace NoteBook.Repository
 
         public async Task<List<Photo>> GetAllByUserIdAsync(int applicationUserId)
         {
-            throw new NotImplementedException();
+            IEnumerable<Photo> photos=new List<Photo>();
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+                photos = await connection.QueryAsync<Photo>(
+                    "Photo_GetByUserId",
+                    new { ApplicationUserId = applicationUserId },
+                    commandType: CommandType.StoredProcedure);
+            }
+            return photos.AsList();
         }
 
         public async Task<Photo> GetAsync(int photoId)
         {
-            throw new NotImplementedException();
+            Photo photo = new Photo();
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+                photo = await connection.QueryFirstOrDefaultAsync<Photo>(
+                    "Photo_Get",
+                    new { PhotoId = photoId },
+                    commandType: CommandType.StoredProcedure);
+            }
+            return photo;
         }
 
         public async Task<Photo> InsertAsync(PhotoCreate photoCreate, int applicationUserId)
         {
-            throw new NotImplementedException();
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("PublicId", typeof(string));
+            dataTable.Columns.Add("ImageUrl", typeof(string));
+            dataTable.Columns.Add("Description", typeof(string));
+            
+            dataTable.Rows.Add(photoCreate.PublicId,photoCreate.ImageUrl,photoCreate.Description);
+
+            int newPhotoId;
+
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+                newPhotoId = await connection.ExecuteScalarAsync<int>(
+                    "Photo_Insert",
+                    new { Photo = dataTable.AsTableValuedParameter("dbo.PhotoType")},
+                    commandType: CommandType.StoredProcedure);
+            }
+
+            Photo photo = await GetAsync(newPhotoId);
+            return photo;
+
         }
     }
 }
